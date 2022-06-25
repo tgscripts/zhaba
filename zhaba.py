9891db0574fa6b0ca5e984c310e14f1337dd96c0
I love you <91001207+kramiikk@users.noreply.github.com>
2022-06-25 14:39:51 +0600

    update zhaba.py

--- zhaba.py
+++ zhaba.py
@@ -529,7 +529,9 @@ class ZhabaMod(loader.Module):
                     for n in range(3):
                         s += 13
                         i = random.randint(13, s)
-                        if (ct.minute + i) > 48:
+                        if (ct.minute + i) > 49:
+                            i += ct.minute +10
+                        else:
                             i = 49 - ct.minute
                         await asyncio.sleep(random.randint(3, 13))
                         await self.client.send_message(
